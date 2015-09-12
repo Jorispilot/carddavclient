16ac6a608d23bcd9f0d596d86718a1b83da94925
Jorispilot <jorispilot@aquilenet.fr>
2015-09-12 14:52:54 +0200

    Now raises an exception when PROPFIND is bad.

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 setup(
-    name = "carddavclient",
-    packages = ["carddavclient"],
-    version = "0.1",
-    description = "Sync CardDAV ressources with local directory.",
-    author = "Joris Picot",
-    author_email = "joris.picot@aquilenet.fr",
-    url = "http://jpilot.fr",
-    keywords = ["carddav", "vcard"],
-    classifiers = [
-        "Development Status :: 1 - Alpha",
-        "Environment :: Console",
-        "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Communications :: Email :: Address Book",
-        ## Todo License
-    ])
+	name = "carddavclient",
+	packages = ["carddavclient"],
+	version = "0.1",
+	description = "Sync CardDAV ressources with local directory.",
+	author = "Jorispilot",
+	author_email = "jorispilot@aquilenet.fr",
+	url = "https://github.com/Jorispilot/carddavclient",
+	keywords = ["carddav", "vcard"],
+	license='MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Environment :: Console",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3 :: Only",
+		"Topic :: Communications :: Email :: Address Book",
+	])
  
